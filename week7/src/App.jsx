c72787cdca090a5e29424564b533f57d054f58a8
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-10-18 22:24:20 +0500

    Week 7 task

---
+++ week7/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Todo from '../src/Pages/Todo'
+
+function App() {
+
+
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
---
+++ week7/src/Pages/Todo.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../Scss/todo.scss';
+
+const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, newTask]);
+      toast.success('Task added!');
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+    toast.error('Task deleted!');
+  };
+
+  return (
+    <div className="todo-container">
+      <h1>Todo App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <ul className="task-list">
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Todo;
